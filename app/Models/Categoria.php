1e397e52a4979ebf89b419eea574a8be526ea189
Elfred Adal Juárez González <elfredjuarezg@gmail.com>
2026-06-26 14:31:14 -0600

    Implementa modelo Categoria y su migración

---
+++ app/Models/Categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = 'categorias';
+    protected $primaryKey = 'idCategoria';
+
+    protected $fillable = [
+        'nombre'
+    ];
+
+    public function materiales()
+    {
+        return $this->hasMany(Material::class, 'categoria_id', 'idCategoria');
+    }
+}
