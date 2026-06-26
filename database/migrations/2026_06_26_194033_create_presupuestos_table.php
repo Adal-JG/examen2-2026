ba488516a065b5df56ac9faafc406d5b436b0f51
Elfred Adal Juárez González <elfredjuarezg@gmail.com>
2026-06-26 14:32:05 -0600

    Implementa modelo Presupuesto y su migración

---
+++ database/migrations/2026_06_26_194033_create_presupuestos_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('presupuestos', function (Blueprint $table) {
+            $table->id('codigoPresupuesto');
+            $table->string('nombrePresupuesto');
+
+            $table->foreignId('unidad_id')
+                ->constrained('unidads', 'idUnidad')
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('presupuestos');
+    }
+};
