bbec6c18f23dde2f05acdbf68d6eaac0b80cb96f
Elfred Adal Juárez González <elfredjuarezg@gmail.com>
2026-06-26 14:31:39 -0600

    Implementa modelo Material y su migración

---
+++ database/migrations/2026_06_26_194031_create_materials_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id('codigo');
+            $table->string('unidadMedida');
+            $table->string('descripcion');
+            $table->string('ubicacion');
+
+            $table->foreignId('categoria_id')
+                ->constrained('categorias', 'idCategoria')
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('materials');
+    }
+};
